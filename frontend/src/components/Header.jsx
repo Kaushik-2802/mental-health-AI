d53a1d9ec19dd3e658f1da02355c29def822eef9
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2025-01-28 18:30:18 +0530

    Enabled location detection through useLocation hook

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { navigation } from "../constants";
 import { brainwave } from "../assets";
+import { useLocation } from "react-router-dom";
 
 /* Last Save : 30.22  */
+
+const pathname = useLocation();
 
 const Header = () => {
   return (
@@ -18,7 +21,11 @@ const Header = () => {
               <a
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1  ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1 "
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:p-12`}
                 key={item.id}
                 href={item.url}
               >
