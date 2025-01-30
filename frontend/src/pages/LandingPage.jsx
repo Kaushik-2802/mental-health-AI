3df0c1f8b6a168a234ce9185542fce9faedf6061
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2025-01-30 12:25:35 +0530

    Added team section

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,12 @@
 import ButtonGradient from "../assets/svg/ButtonGradient";
 import Benefits from "../components/Benifits";
 import Collaboration from "../components/Collaboration";
+import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Pricing from "../components/Pricing";
 import Roadmap from "../components/Roadmap";
-import Services from "../components/Services";
 
 const LandingPage = () => {
   return (
@@ -19,6 +19,7 @@ const LandingPage = () => {
 
         <Pricing />
         <Roadmap />
+        <Contact />
         <Footer />
       </div>
 
