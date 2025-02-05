d236f44504f256059152ea5e0ac2db049bb27eb8
sohamchitimali <sohamchitimalipro@gmail.com>
2025-02-05 19:37:08 +0530

    chat page without styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SignInPage from "./pages/SignInPage";
+import ChatPage from "./pages/ChatPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/chat" element={<LandingPage />} />
+        <Route path="/chat" element={<ChatPage />} />
         <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </Router>
