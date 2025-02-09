8e9d094f383efde07123bc0af09e137873420418
sohamchitimali <sohamchitimalipro@gmail.com>
2025-02-09 12:07:09 +0530

    few chat page fixes

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -117,8 +117,11 @@ function ChatPage() {
               <Settings size={20} className="text-gray-400" />
             </button>
           </div>
+          
 
           <div className="space-y-4">
+
+            
             {chats.map((chat) => (
               <div 
                 key={chat.id} 
@@ -140,6 +143,15 @@ function ChatPage() {
                 )}
               </div>
             ))}
+
+            <button 
+            className="bg-purple-800 text-white text-2xl p-2 rounded-lg mx-auto inline-flex w-full justify-center" 
+            onClick={() => navigate("/report")}
+               >
+             <img src={brainwaveSymbol}  className="w-10 h-10 rounded-full object-cover mr-1" />
+             <div className="mt-0.5">Report</div>
+
+          </button>
           </div>
         </div>
       </div>
@@ -189,17 +201,20 @@ function ChatPage() {
           >
             <Send size={20} />
           </button>
+
+          <button 
+            className="bg-purple-800 text-white text-xl p-2 rounded-lg mx-auto inline-flex " 
+            onClick={() => navigate("/report")}
+          >
+             <img src={brainwaveSymbol}  className="w-8 h-8 rounded-full object-cover mr-1" />
+             <div className="mt-0.5">Report</div>
+          </button>
+
         </form>
 
-        {/* View Report Button */}
-        {analysisData && (
-          <button 
-            className="bg-purple-800 text-white p-2 rounded-lg mx-auto mt-4 mb-4" 
-            onClick={() => navigate("/report", { state: { analysisData } })}
-          >
-            View Report
-          </button>
-        )}
+        
+         
+        
       </div>
     </div>
   );
