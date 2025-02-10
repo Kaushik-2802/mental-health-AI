6c6b9d272880200aed92b8328cb4d12ad6728132
kaushik <kaushikkomaravolu@gmail.com>
2025-02-10 14:39:28 +0530

    Removed backend folder

--- backend/models/Message.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const messageSchema = new mongoose.Schema({
-  timestamp: { type: Date, default: Date.now },
-  msg: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Message", messageSchema);
