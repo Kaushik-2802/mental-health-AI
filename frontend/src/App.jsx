42970ab8a8f5c051af33066e52311cba7826be48
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2025-02-21 14:59:17 +0530

    Route for sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignInPage from "./pages/SignInPage";
 import ChatPage from "./pages/ChatPage";
 import ReportPage from "./pages/ReportPage";
 import DoctorDashboardPage from "./pages/DoctordashboardPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
   return (
@@ -11,9 +12,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<ChatPage />} />
-        <Route path="/dashboard" element={<DoctorDashboardPage/>} />
+        <Route path="/dashboard" element={<DoctorDashboardPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/report" element={<ReportPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
   );
