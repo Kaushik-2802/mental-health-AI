9f90c88a8954329c0ea49a8f1d62ffc9d9490687
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2025-03-18 05:27:10 +0530

    Added server live site for deploy

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -31,12 +31,15 @@ function SignUpPage() {
     }
 
     try {
-      const response = await axios.post("http://localhost:5000/register", {
-        firstname: formData.firstname,
-        surname: formData.surname,
-        email: formData.email,
-        password: formData.password,
-      });
+      const response = await axios.post(
+        "https://mental-health-ai-rilr.onrender.com/register",
+        {
+          firstname: formData.firstname,
+          surname: formData.surname,
+          email: formData.email,
+          password: formData.password,
+        }
+      );
 
       setSuccessMessage(response.data.message);
       setFormData({
